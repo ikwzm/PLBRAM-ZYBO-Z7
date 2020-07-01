56b2fad7e4ad97db4f2d5671f03e9f882337ce96
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2020-07-01 15:33:19 +0900

    [change] to use uiomem from u-dma-buf

--- plbram_test.c
+++ plbram_test.c
@@ -1,153 +1,251 @@
 #include        <stdio.h>
+#include        <stdlib.h>
 #include        <fcntl.h>
 #include        <string.h>
 #include        <time.h>
+#include        <unistd.h>
 #include        <sys/types.h>
+#include        <sys/time.h>
 #include        <sys/mman.h>
 #include        <sys/utsname.h>
 
 static int  _sys_sync_for_cpu_file = -1;
 static int  _sys_sync_for_dev_file = -1;
-static int  _sys_sync_enable       =  0;
+const  int  _SYS_DMA_BIDIRECTIONAL =  0;
+const  int  _SYS_DMA_TO_DEVICE     =  1;
+const  int  _SYS_DMA_FROM_DEVICE   =  2;
 
-static int  _sys_sync_attr(char* attr, unsigned int offset, unsigned int size, int direction)
+static int  _sys_sync_command(char* buf, unsigned int offset, unsigned int size, int direction)
 {
-    return sprintf(attr, "0x%08X%08X\n", offset, ((size & 0xFFFFFFF0) |
-                                                  (direction << 2) | 1));
+    return sprintf(buf, "0x%08X%08X\n", offset, ((size & 0xFFFFFFF0) |
+                                                 (direction << 2) | 1));
+}
+static void _sys_sync_write(int file, char* attr, size_t len)
+{
+    size_t status;
+    status = write(file, attr, len);
 }
 
-void print_diff_time(struct timeval start_time, struct timeval end_time)
+static void diff_time(struct timeval* run_time, struct timeval* start_time, struct timeval* end_time)
 {
-    struct timeval diff_time;
-    if (end_time.tv_usec < start_time.tv_usec) {
-        diff_time.tv_sec  = end_time.tv_sec  - start_time.tv_sec  - 1;
-        diff_time.tv_usec = end_time.tv_usec - start_time.tv_usec + 1000*1000;
+    if (end_time->tv_usec < start_time->tv_usec) {
+        run_time->tv_sec  = end_time->tv_sec  - start_time->tv_sec  - 1;
+        run_time->tv_usec = end_time->tv_usec - start_time->tv_usec + 1000*1000;
     } else {
-        diff_time.tv_sec  = end_time.tv_sec  - start_time.tv_sec ;
-        diff_time.tv_usec = end_time.tv_usec - start_time.tv_usec;
+        run_time->tv_sec  = end_time->tv_sec  - start_time->tv_sec ;
+        run_time->tv_usec = end_time->tv_usec - start_time->tv_usec;
     }
-    printf("time = %ld.%06ld sec\n", diff_time.tv_sec, diff_time.tv_usec);
 }
 
-void memory_write_test(unsigned int size, int o_sync)
+void udmabuf_mmap_write_test(void* buf, unsigned int size, int sync, struct timeval* run_time)
 {
     int            fd;
-    unsigned char  attr[1024];
-    int            attr_len;
-    struct timeval start_time, phase1_time, phase2_time, end_time;
-    int            error_count;
-    unsigned char* udmabuf;
-    unsigned long* tmp_buf;
-    unsigned long  tmp_word  = (O_SYNC == 0)? 0xCC : 0xCD;
-    size_t         tmp_words = size/sizeof(unsigned long);
+    unsigned char  sync_cmd[1024];
+    int            sync_len;
+    struct timeval start_time, end_time;
+    void*          udmabuf;
 
-    if (o_sync == 0)
-        attr_len = _sys_sync_attr(attr, 0, size, 0);
+    if (sync == 0)
+        sync_len = _sys_sync_command(sync_cmd, 0, size, _SYS_DMA_TO_DEVICE);
       
-    tmp_buf = (unsigned long*)malloc(size);
-    if (tmp_buf == NULL) {
-        printf("Can not alloc tmp_buf\n");
-        return;
-    }
-    for (int i = 0; i < tmp_words; i++)
-        tmp_buf[i] = 0;
-
-    if ((fd  = open("/dev/udmabuf0", O_RDWR | o_sync)) != -1) {
+    if ((fd  = open("/dev/uiomem0", O_RDWR | ((sync)?O_SYNC:0))) != -1) {
         udmabuf = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
         gettimeofday(&start_time, NULL);
-        if ((o_sync == 0) && (_sys_sync_enable != 0))
-            write(_sys_sync_for_cpu_file, attr, attr_len);
-        memcpy(udmabuf, tmp_buf, size);
-        if ((o_sync == 0) && (_sys_sync_enable != 0))
-            write(_sys_sync_for_dev_file, attr, attr_len);
+        if (sync == 0)
+            _sys_sync_write(_sys_sync_for_cpu_file, sync_cmd, sync_len);
+        memcpy(udmabuf, buf, size);
+        if (sync == 0)
+            _sys_sync_write(_sys_sync_for_dev_file, sync_cmd, sync_len);
         gettimeofday(&end_time  , NULL);
-        print_diff_time(start_time, end_time);
-        close(fd);
+        diff_time(run_time, &start_time, &end_time);
+        (void)close(fd);
     }
-    free(tmp_buf);
 }
 
-void memory_read_test(unsigned int size, int o_sync)
+void udmabuf_mmap_read_test(void* buf, unsigned int size, int sync, struct timeval* run_time)
 {
     int            fd;
-    unsigned char  attr[1024];
-    int            attr_len;
-    struct timeval start_time, phase1_time, phase2_time, end_time;
-    int            error_count;
-    unsigned char* udmabuf;
-    unsigned long* tmp_buf;
-    size_t         tmp_words = size/sizeof(unsigned long);
+    unsigned char  sync_cmd[1024];
+    int            sync_len;
+    struct timeval start_time, end_time;
+    void*          udmabuf;
 
-    tmp_buf = (unsigned long*)malloc(size);
-    if (tmp_buf == NULL) {
-        printf("Can not alloc tmp_buf\n");
-        return;
-    }
-
-    if (o_sync == 0)
-        attr_len = _sys_sync_attr(attr, 0, size, 0);
+    if (sync == 0)
+        sync_len = _sys_sync_command(sync_cmd, 0, size, _SYS_DMA_FROM_DEVICE);
       
-    if ((fd  = open("/dev/udmabuf0", O_RDWR | o_sync)) != -1) {
+    if ((fd  = open("/dev/uiomem0", O_RDWR | ((sync)?O_SYNC:0))) != -1) {
         udmabuf = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-        printf("MMAP=0x%08X_%08X ", ((unsigned long)udmabuf >> 32) & 0xFFFFFFFF,
-                                    ((unsigned long)udmabuf >>  0) & 0xFFFFFFFF);
         gettimeofday(&start_time, NULL);
-        if ((o_sync == 0) && (_sys_sync_enable != 0))
-            write(_sys_sync_for_cpu_file, attr, attr_len);
-        memcpy(tmp_buf, udmabuf, size);
-        if ((o_sync == 0) && (_sys_sync_enable != 0))
-            write(_sys_sync_for_dev_file, attr, attr_len);
+        if (sync == 0)
+            _sys_sync_write(_sys_sync_for_cpu_file, sync_cmd, sync_len);
+        memcpy(buf, udmabuf, size);
+        if (sync == 0)
+            _sys_sync_write(_sys_sync_for_dev_file, sync_cmd, sync_len);
         gettimeofday(&end_time  , NULL);
-        print_diff_time(start_time, end_time);
+        if (run_time != NULL)
+            diff_time(run_time, &start_time, &end_time);
         close(fd);
     }
-    free(tmp_buf);
 }
 
 void main()
 {
     int            fd;
-    unsigned char  attr[1024];
     unsigned char* buf;
     unsigned int   buf_size;
-    unsigned long  debug_vma = 0;
-    unsigned long  sync_mode = 2;
-    int            error_count;
-    struct timeval start_time, end_time;
+    void*          null_buf = NULL;
+    void*          src0_buf = NULL;
+    void*          src1_buf = NULL;
+    void*          temp_buf = NULL;
 
-    if ((fd  = open("/sys/class/u-dma-buf/udmabuf0/size"     , O_RDONLY)) != -1) {
+    if ((fd  = open("/sys/class/uiomem/uiomem0/size"     , O_RDONLY)) != -1) {
+        char attr[1024];
         read(fd, attr, 1024);
         sscanf(attr, "%d", &buf_size);
         close(fd);
     }
     printf("size=%d\n", buf_size);
 
-    if ((_sys_sync_for_cpu_file = open("/sys/class/u-dma-buf/udmabuf0/sync_for_cpu", O_RDWR)) < 0) {
-        printf("Can not open /sys/class/u-dma-buf/udmabuf0/sync_for_cpu\n");
+    if ((_sys_sync_for_cpu_file = open("/sys/class/uiomem/uiomem0/sync_for_cpu", O_RDWR)) < 0) {
+        printf("Can not open /sys/class/uiomem/uiomem0/sync_for_cpu\n");
         goto done;
     }
     
-    if ((_sys_sync_for_dev_file = open("/sys/class/u-dma-buf/udmabuf0/sync_for_device", O_RDWR)) < 0) {
-        printf("Can not open /sys/class/u-dma-buf/udmabuf0/sync_for_device\n");
+    if ((_sys_sync_for_dev_file = open("/sys/class/uiomem/uiomem0/sync_for_device", O_RDWR)) < 0) {
+        printf("Can not open /sys/class/uiomem/uiomem0/sync_for_device\n");
         goto done;
     }
-    
-    if (1) {
-        printf("memory_write_test(SIZE=%d, O_SYNC=0) : ", buf_size);
-        memory_write_test(buf_size, 0);
 
-        printf("memory_read_test (SIZE=%d, O_SYNC=0) : ", buf_size);
-        memory_read_test(buf_size, 0);
+    if ((null_buf = malloc(buf_size)) == NULL) {
+        printf("Can not malloc null_buf\n");
+        goto done;
+    } else {
+        memset(null_buf, 0, buf_size);
     }
 
-    if (0) {
-        printf("memory_write_test(SIZE=%d, O_SYNC=1) : ", buf_size);
-        memory_write_test(buf_size, O_SYNC);
-
-        printf("memory_read_test (SIZE=%d, O_SYNC=1) : ", buf_size);
-        memory_read_test(buf_size, O_SYNC);
+    if ((src0_buf = malloc(buf_size)) == NULL) {
+        printf("Can not malloc src0_buf\n");
+        goto done;
+    } else {
+        int*   word  = (int *)src0_buf;
+        size_t words = buf_size/sizeof(int);
+        for(int i = 0; i < words; i++) {
+            word[i] = i;
+        }
     }
+
+    if ((src1_buf = malloc(buf_size)) == NULL) {
+        printf("Can not malloc src1_buf\n");
+        goto done;
+    } else {
+        int*   word  = (int *)src1_buf;
+        size_t words = buf_size/sizeof(int);
+        for(int i = 0; i < words; i++) {
+            word[i] = ~i;
+        }
+    }
+
+    if ((temp_buf = malloc(buf_size)) == NULL) {
+        printf("Can not malloc temp_buf\n");
+        goto done;
+    } else {
+        memset(temp_buf, 0, buf_size);
+    }
+
+    {
+        struct timeval run_time;
+        int            w_sync = 1;
+        int            r_sync = 1;
+
+        memset(temp_buf, 0, buf_size);
+
+        printf("mmap write test(SIZE=%d, O_SYNC=%d) : ", buf_size, w_sync);
+        udmabuf_mmap_write_test(src0_buf, buf_size, w_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        printf("mmap read  test(SIZE=%d, O_SYNC=%d) : ", buf_size, r_sync);
+        udmabuf_mmap_read_test (temp_buf, buf_size, r_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        if (memcmp(temp_buf, src0_buf, buf_size) != 0)
+            printf("compare = mismatch\n");
+        else
+            printf("compare = ok\n");
+    }
+
+    {
+        struct timeval run_time;
+        int            w_sync = 1;
+        int            r_sync = 0;
+
+        memset(temp_buf, 0, buf_size);
+        
+        printf("mmap write test(SIZE=%d, O_SYNC=%d) : ", buf_size, w_sync);
+        udmabuf_mmap_write_test(src0_buf, buf_size, w_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        printf("mmap read  test(SIZE=%d, O_SYNC=%d) : ", buf_size, r_sync);
+        udmabuf_mmap_read_test (temp_buf, buf_size, r_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        if (memcmp(temp_buf, src0_buf, buf_size) != 0)
+            printf("compare = mismatch\n");
+        else
+            printf("compare = ok\n");
+    }
+
+    {
+        struct timeval run_time;
+        int            w_sync = 0;
+        int            r_sync = 1;
+
+        memset(temp_buf, 0, buf_size);
+        
+        printf("mmap write test(SIZE=%d, O_SYNC=%d) : ", buf_size, w_sync);
+        udmabuf_mmap_write_test(src0_buf, buf_size, w_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        printf("mmap read  test(SIZE=%d, O_SYNC=%d) : ", buf_size, r_sync);
+        udmabuf_mmap_read_test (temp_buf, buf_size, r_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        if (memcmp(temp_buf, src0_buf, buf_size) != 0)
+            printf("compare = mismatch\n");
+        else
+            printf("compare = ok\n");
+    }
+
+    {
+        struct timeval run_time;
+        int            w_sync = 0;
+        int            r_sync = 0;
+
+        memset(temp_buf, 0, buf_size);
+        
+        printf("mmap write test(SIZE=%d, O_SYNC=%d) : ", buf_size, w_sync);
+        udmabuf_mmap_write_test(src0_buf, buf_size, w_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        printf("mmap read  test(SIZE=%d, O_SYNC=%d) : ", buf_size, r_sync);
+        udmabuf_mmap_read_test (temp_buf, buf_size, r_sync, &run_time);
+        printf("time = %ld.%06ld sec\n", run_time.tv_sec, run_time.tv_usec);
+
+        if (memcmp(temp_buf, src0_buf, buf_size) != 0)
+            printf("compare = mismatch\n");
+        else
+            printf("compare = ok\n");
+    }
+
+
  done:
+    if (temp_buf != NULL)
+        free(temp_buf);
+    if (src1_buf != NULL)
+        free(src1_buf);
+    if (src0_buf != NULL)
+        free(src0_buf);
+    if (null_buf != NULL)
+        free(null_buf);
     if (_sys_sync_for_cpu_file >= 0)
         close(_sys_sync_for_cpu_file);
     if (_sys_sync_for_dev_file >= 0)
